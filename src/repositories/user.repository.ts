4473a6c670343052d09532fafd1a9148619492cc
thiago soares <thiago.98soares6680@hotmail.com>
2022-08-09 17:26:01 -0300

    incluindo banco de dados na aplicação

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,23 @@
+import User from "../models/user.model";
+import db from "../db";
+
+
+
+class UserRepository {
+
+    async findAllUsers():Promise<User[]> {
+        const query = `
+            SELECT uuid, username
+            FROM aplication_user
+        `;
+
+        const result = await db.query<User>(query);
+
+        const rows = result.rows;
+        return rows || [];
+    }
+
+}
+
+
+export default new UserRepository();
--- src/routes/status.route.ts
+++ src/routes/status.route.ts
@@ -3,7 +3,7 @@ import { StatusCodes } from "http-status-codes";
 
 const statusRoute =  Router()
 
-statusRoute.get('/status', (req: Request, res:Response, next:NextFunction) =>{
+statusRoute.get('/status', (req: Request, res:Response, next:NextFunction) => {
     res.sendStatus(StatusCodes.OK)
 })
 

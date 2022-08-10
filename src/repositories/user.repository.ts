3b8842a36d6a0ca8a550d9dfa522baf1b77a0973
thiago soares <thiago.98soares6680@hotmail.com>
2022-08-10 15:52:19 -0300

    cadastro de usuario no BD

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -5,16 +5,76 @@ import db from "../db";
 
 class UserRepository {
 
+    // Consultar todos usuarios
+
     async findAllUsers():Promise<User[]> {
         const query = `
             SELECT uuid, username
             FROM aplication_user
         `;
 
-        const result = await db.query<User>(query);
-
-        const rows = result.rows;
+        const {rows} = await db.query<User>(query);
         return rows || [];
+    }
+
+    // Consultar usurios por ID
+
+    async findById(uuid: string): Promise<User>{
+        const query = `
+            SELECT uuid, username
+            FROM aplication_user
+            WHERE uuid = $1
+        `;
+        const values = [uuid];
+        const { rows } = await db.query<User>(query, values);
+        const [user] = rows;
+        return user;
+    }
+
+    // Criação de usuarios
+
+    async create(user: User): Promise<string>{
+        const script = `
+        INSERT INTO aplication_user (
+            username,
+            password
+        )
+        VALUES ($1, crypt($2, '$my_salt'))
+        RETURNING uuid
+        `;
+        const values = [user.username, user.password];
+
+        const {rows} =  await db.query<{uuid: string}>(script, values);
+        const [newUser] = rows;
+        return newUser.uuid;
+    }
+
+    // Alteração de usuarios
+
+    async update(user: User): Promise<void>{
+        const script = `
+        UPDATE aplication_user 
+        SET
+            username = $1,
+            password = crypt($2, 'my_salt')
+        WHERE uuid = $3
+        
+        `;
+        const values = [user.username, user.password, user.uuid];
+        await db.query(script, values);
+    }
+
+    // Remover usuarios
+
+    async remove(uuid: string): Promise<void>{
+        const cript = `
+        DELETE
+        FROM aplication_user
+        WHERE uuid = $1
+        `;
+        const values = [uuid]
+
+        await db.query(cript, values);
     }
 
 }
